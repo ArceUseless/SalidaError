4472bb4a03da0e38270125e9ad66aaab86334cc0
rafa245 <36188730+rafa245@users.noreply.github.com>
2019-02-26 10:28:33 +0100

    1.1

--- SalidaErrorJava.java
+++ SalidaErrorJava.java
@@ -1,11 +1,11 @@
-/** Este programa ayuda a entender quÈ es la salida estandar y la salida de error y cÛmo manejarlas en java, se divide en tres bloques:
- *   - El primer bloque enviar· un mensaje de prueba por la salida estandar y la de error para enseÒar cuales son por defecto.
- *   - El segundo bloque generar· un fichero el cual ser· la nueva salida de error y se enviar·n de nuevo otros dos mensajes de prueba a cada salida.
- *   - El tercer bloque revertir· la salida de error a su estado original (la consola) y enviar· otros dos mensajes de prueba.
+/** Este programa ayuda a entender qu√© es la salida estandar y la salida de error y c√≥mo manejarlas en java, se divide en tres bloques:
+ *   - El primer bloque enviar√° un mensaje de prueba por la salida estandar y la de error para ense√±ar cuales son por defecto.
+ *   - El segundo bloque generar√° un fichero el cual ser√° la nueva salida de error y se enviar√°n de nuevo otros dos mensajes de prueba a cada salida.
+ *   - El tercer bloque revertir√° la salida de error a su estado original (la consola) y enviar√° otros dos mensajes de prueba.
  *   
- *   Nota: En el bloque dos hay una linea comentada que provocar· una excepciÛn, descomentela si quiere ver cÛmo se envÌa al fichero el mensaje de error de la excepciÛn.
+ *   Nota: En el bloque dos hay una linea comentada que provocar√° una excepci√≥n, descomentela si quiere ver c√≥mo se env√≠a al fichero el mensaje de error de la excepci√≥n.
  *   
- * @author Rafael Jes˙s Nieto Cardador
+ * @author Rafael Jes√∫s Nieto Cardador
 */
 
 import java.io.FileNotFoundException;
@@ -17,8 +17,8 @@ public class SalidaErrorJava {
     //Primer bloque
       
       //Enviamos mensajes por la salida estandar y de error
-      System.err.println("Estamos enviando este mensaje a la salida de error.\n");
-      System.out.println("Estamos enviando este mensaje a la salida estandar.");
+      System.out.println("Estamos enviando este mensaje a la salida estandar.\n");
+      System.err.println("Estamos enviando este mensaje a la salida de error.");
       
     //Segundo bloque
       
@@ -27,15 +27,17 @@ public class SalidaErrorJava {
       //Se cambia la salida de errores al fichero que hemos creado.
       System.setErr(new PrintStream(ficheroError));
       
-      System.err.println("Estamos enviando este mensaje a la salida de error.");
       System.out.println("Estamos enviando este mensaje a la salida estandar.\n");
+      //La linea de abajo es una excepci√≥n opcional, descomentela si quiere ver c√≥mo influye
+      //int i = 1/0;
+      System.err.println("Estamos enviando este mensaje a la salida de error.");
       
     //Tercer bloque
       
       //Hacemos que la salida de error sea la misma que la salida estandar
       System.setErr(new PrintStream(System.out));
         
-      System.err.println("Estamos enviando este mensaje a la salida de error.");
       System.out.println("Estamos enviando este mensaje a la salida estandar.");
+      System.err.println("Estamos enviando este mensaje a la salida de error.");
   }
 }
